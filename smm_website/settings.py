9b7672d3c283f7724eec759c3482079ed75d0cff
Nick Ilin <ninikitos90@gmail.com>
2024-03-04 23:15:25 -0500

    debug false

--- smm_website/settings.py
+++ smm_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("IS_DEBUG")
+DEBUG = False
 
 # ALLOWED_HOSTS = [os.getenv("APP_HOST")]
 ALLOWED_HOSTS = ["*"]
