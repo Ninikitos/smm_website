99995cbf7ab2adfa079a4f59bb8e4f2ce66b9bea
Nick Ilin <ninikitos90@gmail.com>
2024-03-13 22:20:15 -0400

    captcha

--- smm_website/settings.py
+++ smm_website/settings.py
@@ -147,4 +147,3 @@ EMAIL_HOST_PASSWORD = "uuje xqmh kezf agwb"
 #reCAPTCHA
 RECAPTCHA_PUBLIC_KEY = '6LeAI5gpAAAAANaXePL9X-glaaxh81FhL4ixZkyC'
 RECAPTCHA_PRIVATE_KEY = '6LeAI5gpAAAAAGw2k2bwppdaC8uygIR_Yk6rI1T6'
-RECAPTCHA_PROXY = {'http': 'http://hypermediapro.com', 'https': 'https://hypermediapro.com'}
